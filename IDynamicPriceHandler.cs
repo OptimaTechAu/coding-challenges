e59c8c9a277ae9a899ef9d7184476d83ba04e9d7
RenChand <96213851+RenChand@users.noreply.github.com>
2022-12-08 10:01:24 +1100

    Add files via upload

---
+++ IDynamicPriceHandler.cs
@@ -0,0 +1,59 @@
+﻿namespace Renu.Supermarket.Kata
+{
+    public interface IDynamicPriceHandler
+    {
+        decimal GetTotal(Sku sku, int count);
+    }
+
+    public class DynamicPriceHandler : IDynamicPriceHandler
+    {
+        private readonly ISkuOfferRepo _skuOfferRepo;
+
+        public DynamicPriceHandler(ISkuOfferRepo skuOfferRepo)
+        {
+            _skuOfferRepo = skuOfferRepo;
+        }
+
+        decimal IDynamicPriceHandler.GetTotal(Sku sku, int itemCount)
+        {
+            var discounts = _skuOfferRepo.GetOffers(sku)?.OrderByDescending(x => x.Count).ToList();
+
+            if (discounts != null && discounts.Any())
+            {
+                ISkuSpecialPrice? specialPrice = null;
+                decimal price = 0;
+                int i = 0;
+
+                int total = discounts.Count();
+                int remaingItemCount = itemCount;
+
+                do
+                {
+                    for (; i <= total; i++)
+                    {
+                        if (remaingItemCount >= discounts[i].Count)
+                        {
+                            specialPrice = discounts[i];
+                            i++;
+                            break;
+                        }
+                    }
+
+                    price += (remaingItemCount / specialPrice!.Count) * specialPrice.Amount;
+
+                    remaingItemCount = (remaingItemCount % specialPrice!.Count);
+
+                } while (remaingItemCount > 1);
+
+                if (remaingItemCount > 0)
+                {
+                    price += remaingItemCount * sku.Price;
+                }
+
+                return price;
+            }
+
+            return sku.Price * itemCount;
+        }
+    }
+}
---
+++ ISkuSpecialPrice.cs
@@ -0,0 +1,23 @@
+﻿namespace Renu.Supermarket.Kata
+{
+    public interface ISkuSpecialPrice
+    {
+        int Id { get; }
+        int Count { get; }
+        decimal Amount { get; }
+    }
+
+    internal class SkuSpecialPrice : ISkuSpecialPrice
+    {
+        public SkuSpecialPrice(int count, decimal amount)
+        {
+            Count = count;
+            Amount = amount;    
+            Id = Guid.NewGuid().GetHashCode();
+        }
+
+        public int Id { get; private set; }
+        public int Count { get; private set; }
+        public decimal Amount { get; private set; }
+    }
+}
